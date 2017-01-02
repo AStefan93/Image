11a670784b6888760058bc1251c87406aa872167
Stefan Arnautu <arnautu.stefan93@gmail.com>
2017-01-02 20:39:30 +0200

    Minor modifications to main file.

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ namespace imgintf = ImageProc::CIMGInterface;
 
 int main(int, char**) { 
 
-//	char const* image_path = "test/lena.bmp";
-	char const* image_path2 = "test/stitching/IMG_3204.jpg";
+	char const* image_path2 = "test/lena_baseline_grayscale.jpg";
+//	char const* image_path2 = "test/stitching/IMG_3204.jpg";
 //	char const* image_path2 = "test/JPEG1.jpg";
 
 	std::clock_t start;
